cc04467c00a6bc4ea457cb89944afb3195829d2d
Devansh <dewanshvishwakarma073@gmail.com>
2026-05-26 19:15:39 +0530

    longest sub string(sliding window)

---
+++ src/Sliding_window/LC3_longest_repeating_sub_string.java
@@ -0,0 +1,53 @@
+package Sliding_window;
+
+import java.util.HashSet;
+import java.util.Set;
+//approach 1 with the help of Hashset
+public class LC3_longest_repeating_sub_string {
+    static int Str(String s){
+        Set<Character> set=new HashSet<>();
+        int n=s.length();
+        int maxL=0;
+//        int i=0;
+//        int j=0;
+//        for (int j=0;j<n;j++){
+//            while (set.contains(s.charAt(j))){
+//                set.remove(s.charAt(i));
+//                i++;
+//            }
+//            set.add(s.charAt(j));
+//            maxL=Math.max(maxL,j-i+1);
+//        }
+//        return maxL;
+
+
+
+
+        int windowStart=0;
+        int windowEnd=0;
+        while (windowEnd<n){
+            char ch = s.charAt(windowEnd);
+
+            if (set.contains(ch)){
+                while (windowStart<windowEnd && set.contains(ch)){
+                    set.remove(s.charAt(windowStart));
+                    windowStart++;
+                }
+            }
+            set.add(ch);
+            maxL=Math.max(maxL,windowEnd-windowStart+1);
+            windowEnd++;
+        }
+        return maxL;
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+String s1= "bbbbb";
+int result=Str(s1);
+        System.out.println(result);
+    }
+}
---
+++ src/Sliding_window/Lc3_longest_sub_String.java
@@ -0,0 +1,10 @@
+package Sliding_window;
+
+public class Lc3_longest_sub_String {
+    static void Sub_String(String s){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
