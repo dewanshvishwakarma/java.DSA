9dcd31f4849647da7c9f525a0ba8da8e77d5fd4d
Devansh <dewanshvishwakarma073@gmail.com>
2026-05-27 22:00:05 +0530

    solve longest sub string leetcode problem 3

--- src/Sliding_window/Lc3_longest_sub_String.java
+++ src/Sliding_window/Lc3_longest_sub_String.java
@@ -1,10 +1,27 @@
 package Sliding_window;
 
-public class Lc3_longest_sub_String {
-    static void Sub_String(String s){
+import java.util.HashMap;
 
+public class Lc3_longest_sub_String {
+    static int Sub_String(String s){
+        HashMap<Character,Integer> map= new HashMap<>();
+        int n=s.length();
+        int i=0;
+        int maxL=0;
+        for (int j=0;j<n;j++){
+            char ch=s.charAt(j);
+            if (map.containsKey(ch) && i<=map.get(ch)){
+                i=map.get(ch)+1;
+            }
+            map.put(ch,j);
+            maxL=Math.max(maxL,j-i+1);
+        }
+return maxL;
     }
     public static void main(String[] args) {
-
+    String s1="bbbbb";
+    String s2= "pwwkew";
+    int result=Sub_String(s2);
+        System.out.println(result);
     }
 }
