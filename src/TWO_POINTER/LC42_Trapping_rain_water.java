176474ef6dced773d5a4bf0821e84286aa5e856c
Devansh <dewanshvishwakarma073@gmail.com>
2026-05-29 23:15:40 +0530

    two pointer trapping rain water problem

---
+++ src/TWO_POINTER/LC42_Trapping_rain_water.java
@@ -0,0 +1,27 @@
+package TWO_POINTER;
+
+public class LC42_Trapping_rain_water {
+    static int TRW(int[] a){
+        int n= a.length;
+        int[] L_max=new int[a.length];
+        int[] R_max=new int[a.length];
+        L_max[0]=a[0];
+        R_max[n-1]=a[n-1];
+        for (int i=1;i<a.length;i++){
+           L_max[i]= Math.max(a[i],L_max[i-1]);
+        }
+        for (int j=n-2;j>=0;j--){
+            R_max[j]=Math.max(a[j],R_max[j+1]);
+        }
+
+        int ans=0;
+
+        for (int k=0;k<n;k++){
+            ans=ans+(Math.min(L_max[k],R_max[k])-a[k]);
+        }
+        return ans;
+    }
+    static void main(String[] args) {
+
+    }
+}
