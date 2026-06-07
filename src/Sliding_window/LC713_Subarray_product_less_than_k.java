30ef1ebd283a200579436fec2b7c7b5e005e6d33
Devansh <dewanshvishwakarma073@gmail.com>
2026-06-07 21:00:48 +0530

    LC714 subarray product less than k (variable sliding window) delete i if product>=k

---
+++ src/Sliding_window/LC713_Subarray_product_less_than_k.java
@@ -0,0 +1,25 @@
+package Sliding_window;
+
+import javax.swing.plaf.IconUIResource;
+
+public class LC713_Subarray_product_less_than_k {
+    static int Subarray(int[] a, int k){
+        int i=0;
+        int count=0;
+        long product=1;
+        for (int j=0;j<a.length;j++){
+            product*=a[j];
+            while (product>=k){
+                product=product/a[i];
+                i++;
+            }
+            count=count+j-i+1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] a={10,5,2,6};
+        int result=Subarray(a,100);
+        System.out.println(result);
+    }
+}
