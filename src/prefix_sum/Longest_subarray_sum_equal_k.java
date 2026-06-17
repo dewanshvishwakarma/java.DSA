71baf90d678ff9d110233128208911ba61a91f15
Devansh <dewanshvishwakarma073@gmail.com>
2026-06-17 15:53:27 +0530

    practice prefix (longest sum sub array of k)

---
+++ src/prefix_sum/Longest_subarray_sum_equal_k.java
@@ -0,0 +1,30 @@
+package prefix_sum;
+//https://www.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1
+import java.util.HashMap;
+
+public class Longest_subarray_sum_equal_k {
+    static int Sub(int[] a, int k){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,-1);
+        int prefix=0;
+        int result=0;
+        for (int i=0;i<a.length;i++){
+            prefix=prefix+a[i];
+            int dif=prefix-k;
+
+            if (map.containsKey(dif)){
+               result= Math.max(result,i-map.get(dif));
+            }
+            if (!map.containsKey(prefix)) {
+                map.put(prefix,i);
+            }
+
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] a={5,2,2,5,1,1,1,1,4};
+        int r=Sub(a,4);
+        System.out.println(r);
+    }
+}
