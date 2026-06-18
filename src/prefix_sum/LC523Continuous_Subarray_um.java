ffea38946a99268933e1d5e6a0906ad22d23e9aa
Devansh <dewanshvishwakarma073@gmail.com>
2026-06-18 23:51:30 +0530

    solve(lc523) A good subarray is a subarray where:its length is at least two, and the sum of the elements of the subarray is a multiple of k.

---
+++ src/prefix_sum/LC523Continuous_Subarray_um.java
@@ -0,0 +1,29 @@
+package prefix_sum;
+
+import java.util.HashMap;
+
+public class LC523Continuous_Subarray_um {
+    static boolean continuous(int[] a,int k){
+        HashMap<Integer,Integer> map =new HashMap<>();
+        map.put(0,-1);
+        int sum=0;
+
+        for (int i=0;i<a.length;i++){
+            sum+=a[i];
+            int modulo=sum%k;
+            if (map.containsKey(modulo)){
+                if (i-map.get(modulo)>=2){
+                return true;
+                }
+            }else{
+                map.put(modulo,i);
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+int[] a={7};
+boolean result=continuous(a,6);
+        System.out.println(result);
+    }
+}
