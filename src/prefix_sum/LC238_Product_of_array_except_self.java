88335e031ef38e816a394f42d95c2f8045221408
Devansh <dewanshvishwakarma073@gmail.com>
2026-06-19 23:18:35 +0530

    solve(lC238) product except self learn prefix and suffix product continue learn prefix sum pattern

---
+++ src/prefix_sum/LC238_Product_of_array_except_self.java
@@ -0,0 +1,20 @@
+package prefix_sum;
+
+public class LC238_Product_of_array_except_self {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[n];
+        ans[0]=1;
+        for(int i=1;i<nums.length;i++){
+            ans[i]=ans[i-1]*nums[i-1];}
+        int rightp=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]=ans[i]*rightp;
+            rightp=rightp*nums[i];}
+        return ans;
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
