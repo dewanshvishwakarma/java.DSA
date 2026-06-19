109a3e4a518f7628347778cc9fd7496d7d15b146
Devansh <dewanshvishwakarma073@gmail.com>
2026-06-19 23:36:09 +0530

    solve(lC1248) count nice sub array using hashmap and prefix sum

---
+++ src/prefix_sum/LC1248_Count_Number_of_Nice_Subarrays.java
@@ -0,0 +1,30 @@
+package prefix_sum;
+
+import java.util.HashMap;
+
+public class LC1248_Count_Number_of_Nice_Subarrays {
+    public int numberOfSubarrays(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                nums[i]=0;
+            }else{
+                nums[i]=1;
+            }
+        }
+        int sum=0;
+        int p=0;
+        int r=0;
+        for(int i=0;i<nums.length;i++){
+            p=p+nums[i];
+            int dif=p-k;
+            if(map.containsKey(dif)){
+                r=r+map.get(dif);
+            }
+            map.put(p,map.getOrDefault(p,0)+1);
+
+        }
+        return r;
+    }
+}
