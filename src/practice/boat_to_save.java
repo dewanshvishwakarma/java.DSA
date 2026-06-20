47662f7f11655d4c5d7d92d41f455297e0e0a27c
Devansh <dewanshvishwakarma073@gmail.com>
2026-06-20 23:38:33 +0530

    do revise two pointer patter pattern

---
+++ src/practice/boat_to_save.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class boat_to_save {
+    static int  save(int[] a , int limit){
+       int  boat=0;
+       int i=0;
+       int j=a.length-1;
+
+       while(i<=j){
+           if(a[i]+a[j]<=limit){
+               boat++;
+               i++;
+               j--;
+           }else {
+               boat++;
+               j--;
+           }
+       }
+       return boat;
+    }
+    static void main(String[] args) {
+        int[] a={3,5,3,4};
+        int result=save(a,5);
+        System.out.println(result);
+    }
+}
