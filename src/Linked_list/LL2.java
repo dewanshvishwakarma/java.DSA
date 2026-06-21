15cb4b0cc42f34fcc295a5fe14f8646f902f58f4
Devansh <dewanshvishwakarma073@gmail.com>
2026-06-21 23:11:03 +0530

    learn insertion at first in linked list

---
+++ src/Linked_list/LL2.java
@@ -0,0 +1,34 @@
+package Linked_list;
+// learn insert in first without function
+import java.util.Stack;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public class LL2 {
+    public static void main(String[] args) {
+        Node head=new Node(20);
+        Node b=new Node(30);
+        Node c=new Node(40);
+
+        head.next=b;
+        b.next=c;
+
+        Node newNode=new Node(10);
+        newNode.next=head;
+        head=newNode;
+
+        Node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data  + " ");
+            temp=temp.next;
+        }
+    }
+}
+
