bd31b729f099709cde99143d087a5c852a54f880
Devansh <dewanshvishwakarma073@gmail.com>
2026-06-30 22:48:24 +0530

    gfg_find next greater element in stack using stack

--- src/stack/next_greater_element.java
+++ src/stack/next_greater_element.java
@@ -23,16 +23,24 @@ public class next_greater_element {
         Stack<Integer> st=new Stack<>();
         int n=a.length;
         int[] ans=new int[n];
-        for (int i=n-1;i>=0;i--){
-            while (!st.isEmpty()){
-                x
+        for(int i=n-1;i>=0;i--){
+            while (st.size()>0 && st.peek()<=a[i]) {
+                st.pop();
             }
+                if (st.isEmpty()){
+                    ans[i]=-1;
+                }else{
+                    ans[i]=st.peek();
+                }
+                st.push(a[i]);
+
         }
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] a={6,0,8,1,3};
-        int[] ans=nge(a);
+        int[] a={6,8,0,1,3};
+        int[] ans=NGE(a);
         System.out.println(Arrays.toString(ans));
     }
 }
