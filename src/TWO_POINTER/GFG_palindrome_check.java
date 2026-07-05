be33785f79f8849afd320a9810dbfec1ce77b783
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-06 00:41:38 +0530

    solve array gfg basic CHECK ARRAY IS PALINDROME OR NOT USING TWO POINTER APPROACH;

---
+++ src/TWO_POINTER/GFG_palindrome_check.java
@@ -0,0 +1,4 @@
+package TWO_POINTER;
+//https://www.geeksforgeeks.org/problems/perfect-arrays4645/1?page=2&category=Arrays,Java&difficulty=Basic&sortBy=submissions
+public class GFG_palindrome_check {
+}
