0e9caaf58923ed8232d674d4adc8b7b1436a99d0
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-05 19:01:23 +0530

    make an min stack method in stack that give min element in o(1) time

--- src/stack/min_stack.java
+++ src/stack/min_stack.java
@@ -2,15 +2,45 @@ package stack;
 
 import java.util.Stack;
 
-public class min_stack {
-    static void minS(){
-        Stack<Integer> main=new Stack<>();
-        Stack<Integer> minStack=new Stack<>();
+ public  class min_stack {
+        Stack<Integer> st=new Stack<>();
+        static Stack<Integer>  ms=new Stack<>();
 
+        public void push(int x){
+            if (ms.isEmpty() || x<=ms.peek()){
+                ms.push(x);
+            }
+            st.push(x);
+        }
+
+        public void pop(){
+            if (st.peek().equals(ms.peek())){
+                ms.pop();
+            }
+            st.pop();
+        }
+
+        public int peek(){
+            if (st.isEmpty()){
+                return -1;
+            }
+           return st.peek();
+        }
+
+     static public int minStack(){
+            if (ms.isEmpty()){
+                return -1;
+            }
+            return ms.peek();
+        }
         
-    }
-    public static void main(String[] args) {
+    public static   void main(String[] args) {
+        min_stack m = new min_stack();
 
-
+        m.push(4);
+        m.push(5);
+        m.push(-2);
+        int n=minStack();
+        System.out.println(n);
     }
-}
+        }
