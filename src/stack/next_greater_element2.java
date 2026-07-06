9cefbbe0c2a248bebbe04559019232d61b148198
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-06 07:09:56 +0530

    next greather element brute force understand

--- src/stack/next_greater_element2.java
+++ src/stack/next_greater_element2.java
@@ -1,8 +1,37 @@
 package stack;
 
+import java.util.Arrays;
 import java.util.Stack;
 //https://leetcode.com/problems/next-greater-element-ii/description/
 public class next_greater_element2 {
+    static int[] nextGreaterElement(int[] a, int[] b){
+        int m=a.length;
+        int n=b.length;
+        int[] ans=new int[m];
+
+        //search element index at array b
+        for ( int i=0;i<m;i++){
+            int index=-1;
+            for (int j=0;j<n;j++){
+                if(a[i]==b[j]){
+                    index=j;
+                    break;
+                }
+            }
+            ans[i]=-1;
+            for (int j=index+1;j<n;j++){
+                if (b[j]>a[i]){
+                    ans[i]=b[j];
+                    break;
+                }
+            }
+
+        }
+return ans;
+
+    }
+
+
     static int[] NGE2(int [] a){
         int n=a.length;
         int[] ans=new int[n];
@@ -22,5 +51,12 @@ public class next_greater_element2 {
             s.push(c);
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] a={4,1,2};
+        int[ ] b={1,3,4,2};
+        int[] ans=nextGreaterElement(a,b);
+        System.out.println(Arrays.toString(ans));
     }
 }
