fb1ddab117ca72eab792fdb98cb5873fb4a0a66c
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-08 08:11:05 +0530

    LC 739 (stack) solve daily temperature using stack data structure with next greater pattern

---
+++ src/stack/LC739_daily_temperature.java
@@ -0,0 +1,31 @@
+package stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class LC739_daily_temperature {
+//    we have to given an array of temperature of an days and we find to find the
+//    gap between the next greater temperature
+
+    static int[] dailyTemperatures(int[] a){
+        Stack<Integer> s=new Stack<>();
+        int n=a.length;
+        int[] ans=new int[n];
+
+        for (int i=n-1;i>=0;i--){
+            while (!s.isEmpty() && a[s.peek()]<=a[i]){
+                s.pop();
+            }
+            if (s.isEmpty()){
+                ans[i]=0;
+            }else {
+                ans[i]=s.peek()-i;
+            }
+            s.push(i);
+        }
+        return ans ;
+    }
+
+    public static void main(String[] args) {
+    }
+}
