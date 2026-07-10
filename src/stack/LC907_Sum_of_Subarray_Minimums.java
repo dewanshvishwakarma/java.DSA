931469eb363ee8a7adcc982b792df20a65dcdd3a
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-10 23:16:40 +0530

    LC 907 (stack) find the sum of minimum element in an sub array

---
+++ src/stack/LC907_Sum_of_Subarray_Minimums.java
@@ -0,0 +1,19 @@
+package stack;
+
+public class LC907_Sum_of_Subarray_Minimums {
+//    brute force
+    static int sumOfSubarray(int[] a){
+        int sum=0;
+        for (int i=0;i<a.length;i++){
+            int min=a[i];
+            for (int j=i;j< a.length;j++){
+                min=Math.min(min,a[j]);
+                sum+=min;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+
+    }
+}
