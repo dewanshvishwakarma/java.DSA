8feea487900fc761082d4c2d9047e35713cbe041
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-11 13:23:46 +0530

    LC 907 (stack) find the sum of minimum element in an sub array

--- src/stack/LC907_Sum_of_Subarray_Minimums.java
+++ src/stack/LC907_Sum_of_Subarray_Minimums.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.Stack;
+
 public class LC907_Sum_of_Subarray_Minimums {
 //    brute force
 //Start from index i.
@@ -17,7 +19,54 @@ public class LC907_Sum_of_Subarray_Minimums {
         }
         return sum;
     }
+
+    static int sumOfSubarray2(int[] arr){
+        int n=arr.length;
+        Stack<Integer> s=new Stack<>();
+        int[] NSR=new int[n];
+        int[] NSL=new int[n];
+        int sum=0;
+        for(int i=n-1;i>=0;i--){
+            while(!s.isEmpty() && arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                NSR[i]=n;
+            }else{
+                NSR[i]=s.peek();
+            }
+            s.push(i);
+        }
+        s.clear();
+
+// NSL
+        for(int i=0;i<n;i++){
+            while(!s.isEmpty() && arr[s.peek()]>arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                NSL[i]=-1;
+            }else{
+                NSL[i]=s.peek();
+            }
+            s.push(i);
+        }
+
+        for(int i=0;i<n;i++){
+            int left=i-NSL[i];
+            int right=NSR[i]-i;
+            int totalWays=left*right;
+            int totalsum=arr[i]*totalWays;
+            sum=(sum+totalsum);
+        }
+        return sum;
+
+
+    }
     public static void main(String[] args) {
+int[] a={3,1,2,4};
+int sol=sumOfSubarray2(a);
+        System.out.println(sol);
 
     }
 }
