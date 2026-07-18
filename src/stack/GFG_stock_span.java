bd3c85939624f609d6172778fc4fd9348bdaac0d
Devansh <dewanshvishwakarma073@gmail.com>
2026-07-18 22:50:34 +0530

    stock span revision

--- src/stack/GFG_stock_span.java
+++ src/stack/GFG_stock_span.java
@@ -22,10 +22,30 @@ public class GFG_stock_span {
         }
 return ans;
     }
+
+    static int[] Stock_span2(int[] a){
+        int n=a.length;
+        int[] sol=new int[n];
+        Stack<Integer> s=new Stack<>();
+
+        for (int i=0;i<n;i++){
+            while (!s.isEmpty() && a[s.peek()]<=a[i]){
+                s.pop();
+            }
+            if (s.isEmpty()){
+                sol[i]=1;
+            }else{
+                sol[i]=i-s.peek();
+            }
+            s.push(i);
+        }
+        return sol;
+    }
     public static void main(String[] args) {
         int[] a={100,80,90,120};
         int[] ans=Stock_span(a);
+        int[] ans2=Stock_span2(a);
         System.out.println(Arrays.toString(ans));
-
+        System.out.println(Arrays.toString(ans2));
     }
 }
