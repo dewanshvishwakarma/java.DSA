b0857a04dd202c4de15979060abfb278ca09ed39
Devansh <dewanshvishwakarma073@gmail.com>
2026-08-25 20:36:08 +0530

    start 2 d array make 2 d array traverse and print

--- src/two_D_array/Td1.java
+++ src/two_D_array/Td1.java
@@ -5,26 +5,20 @@ import java.util.Scanner;
 public class Td1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+//making the array
         int m = sc.nextInt();
         int n = sc.nextInt();
         int[][] arr = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.println(i + " "+ j);
-
             }
-
+//            print the array
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(arr[i][j]);
-
             }
-
         }
-
-
-
     }
 }
