7a4c89509af633e4734f2ed3676463d7a0076232
Devansh <dewanshvishwakarma073@gmail.com>
2026-08-25 20:21:44 +0530

    start 2 d array

--- src/two_D_array/Td1.java
+++ src/two_D_array/Td1.java
@@ -1,5 +1,21 @@
 package two_D_array;
 
-public class Td1 {
+import java.util.Scanner;
 
+public class Td1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " "+ j);
+
+            }
+
+        }
+
+    }
 }
