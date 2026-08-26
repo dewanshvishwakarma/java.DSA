ec0ac1b8974b06fd788c9d86a45abff8310f4402
Devansh <dewanshvishwakarma073@gmail.com>
2026-08-26 22:45:32 +0530

    start 2 d array transpose

--- src/two_D_array/transpose.java
+++ src/two_D_array/transpose.java
@@ -13,6 +13,12 @@ public class transpose {
                 a[i][j]=sc.nextInt();
             }
         }
-
+//transpose
+     for(int i=0;i<n;i++){
+         for (int j=0;j<m;j++){
+             System.out.print(a[j][i]);
+         }
+         System.out.println();
+     }
     }
 }
