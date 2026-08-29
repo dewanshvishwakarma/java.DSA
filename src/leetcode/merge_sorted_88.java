6e4bbe1b556d9810b78cc4ced2c37adeb27dec4b
Devansh <dewanshvishwakarma073@gmail.com>
2026-08-29 22:00:36 +0530

    merge sorted array simple

---
+++ src/leetcode/merge_sorted_88.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+public class merge_sorted_88 {
+    static int[]  merge(int[] a,int[] b){
+        int m=a.length;
+        int n=b.length;
+        int[] result=new int[m+n];
+        int k=0;
+        int i=0;
+        int j=0;
+        while (i<m && j<n){
+            if (a[i]<=b[j]){
+                result[k]=a[i];
+                k++;
+                i++;
+            }else {
+                result[k]=b[j];
+                j++;
+                k++;
+            }
+        }
+
+        while (i < a.length) {
+            result[k] = a[i];
+            i++;
+            k++;
+        }
+
+        // Remaining elements of b
+        while (j < b.length) {
+            result[k] = b[j];
+            j++;
+            k++;
+        }
+return result;
+
+    }
+    static void main(String[] args) {
+        int[] a={1,2,3};
+        int[] b={1,2,2};
+        int[] r=merge(a,b);
+        for (int i=0;i<r.length;i++){
+            System.out.print(r[i] + " ");
+        }
+    }
+}
