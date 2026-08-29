ae2121f2b1a0f87fc4273d15da6866135ba92cb5
Devansh <dewanshvishwakarma073@gmail.com>
2026-08-29 08:11:23 +0530

    leetcode 283 move zero at end (two pointer)

---
+++ src/leetcode/move_zero283.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class move_zero283 {
+    static void move(int[] a){
+        int n=a.length;
+        int i=0;
+        for (int j=0;j<n;j++){
+            if(a[j]!=0){
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+                i++;
+            }
+        }
+    }
+    static void main(String[] args) {
+        int[] a={0,1,0,3,12};
+        move(a);;
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+ " ");
+        }
+    }
+}
