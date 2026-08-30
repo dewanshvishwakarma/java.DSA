76557544f4d6235399399ab43575fc6c603cdb3d
Devansh <dewanshvishwakarma073@gmail.com>
2026-08-30 21:34:50 +0530

    create an tree class basic implementation

---
+++ src/tree/basic_implemetation.java
@@ -0,0 +1,23 @@
+package tree;
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+public class basic_implemetation {
+    static void main(String[] args) {
+        Node root=new Node(10);
+        root.left=new Node(20);
+        root.right=new Node(30);
+        root.left.left=new Node(40);
+        root.left.right=new Node(50);
+    }
+
+}
