ea5583506a1bb54d8a75921312de004129eae6bf
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-08 09:53:24 +0530

    String basic question

---
+++ src/practice/String_frequency.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class String_frequency {
+    static void main(String[] args) {
+        char ch='A';
+        for (int i=0;i<10;i++){
+            System.out.println(ch++);
+        }
+
+    }
+}
