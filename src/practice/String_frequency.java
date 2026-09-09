c572b5a58432f51efb4a99d6b957c0431f142f84
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-09 07:36:14 +0530

    String operation

--- src/practice/String_frequency.java
+++ src/practice/String_frequency.java
@@ -1,10 +1,15 @@
 package practice;
 
+import java.util.Arrays;
+
 public class String_frequency {
     static void main(String[] args) {
-        char ch='A';
-        for (int i=0;i<10;i++){
-            System.out.println(ch++);
-        }
+//        char ch='A';
+//        for (int i=0;i<10;i++){
+//            System.out.println(ch++);
+//        }
+        String[] colors = {"Red", "Green", "Blue"};
+
+
     }
 }
