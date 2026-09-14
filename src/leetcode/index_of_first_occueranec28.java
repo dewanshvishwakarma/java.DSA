d83bfaae2a94bad3b278563f7dc0069cbf129ff9
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-14 22:52:49 +0530

    leetcode 28(String) two pointer

--- src/leetcode/index_of_first_occueranec28.java
+++ src/leetcode/index_of_first_occueranec28.java
@@ -25,11 +25,8 @@ public class index_of_first_occueranec28 {
     }
 }
 //Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-//
-//
-//
+
 //Example 1:
-//
 //Input: haystack = "sadbutsad", needle = "sad"
 //Output: 0
 //Explanation: "sad" occurs at index 0 and 6.
