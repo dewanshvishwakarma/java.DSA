f79d5f54e269a5f2be51bc5bcbf2535260949e49
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-21 22:14:21 +0530

    linked list question practice

---
+++ src/Linked_list/odd_even.java
@@ -0,0 +1,20 @@
+package Linked_list;
+
+public class odd_even {
+    static Node odd(Node head){
+        Node i=head;
+        Node temp=head.next;
+        Node j=head.next;
+
+        while (j!=null && j.next!=null){
+            i.next=j.next;
+            i=i.next;
+            j.next=i.next;
+            j=j.next;
+        }
+        i.next=temp;
+        return head;
+    }
+    static void main(String[] args) {
+    }
+}
