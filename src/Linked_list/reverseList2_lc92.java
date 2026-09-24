5096f0c51d6192217a103d92d919fe16dc0519f8
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-24 23:34:16 +0530

    leetcode 92 (linked list) use concept of dummy node

--- src/Linked_list/reverseList2_lc92.java
+++ src/Linked_list/reverseList2_lc92.java
@@ -6,6 +6,17 @@ public class reverseList2_lc92 {
         if (head == null || left == right) {
             return head;
         }
+
+        // Dummy node
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        // Move prev to the node just before left
+        ListNode prev = dummy;
+
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
     }
     static void main(String[] args) {
 
