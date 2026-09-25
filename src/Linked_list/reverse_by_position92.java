ebc21e43576e87da0039ae34309591b384659543
Devansh <dewanshvishwakarma073@gmail.com>
2026-09-25 21:23:55 +0530

    linked list revision dummy node

--- src/Linked_list/reverse_by_position92.java
+++ src/Linked_list/reverse_by_position92.java
@@ -1,4 +1,32 @@
 package Linked_list;
 
 public class reverse_by_position92 {
+     if (head == null || left == right) {
+        return head;
+    }
+
+    // Dummy node
+    Node dummy = new ListNode(0);
+    dummy.next = head;
+
+    // Move prev to the node just before left
+    ListNode prev = dummy;
+
+        for (int i = 1; i < left; i++) {
+        prev = prev.next;
+    }
+
+    // Reverse the portion
+    ListNode curr = prev.next;
+
+        for (int i = 0; i < right - left; i++) {
+
+        ListNode next = curr.next;
+
+        curr.next = next.next;
+        next.next = prev.next;
+        prev.next = next;
+    }
+
+        return dummy.next;
 }
